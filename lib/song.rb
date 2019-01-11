174a6b76e2e0e41bc0f62a8a72b485505fddb4c2
Dalya Eidda <dalia.eidda@gmail.com>
2019-01-11 22:34:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,6 +29,11 @@ class Song
   end 
   
   def self.find_or_create_by_name(name)
+    if find_by_name(name) == nil 
+      find_by_name(name)
+    else 
+      find_by_name(name )
+    end 
   end 
   
   def self.alphabetical
