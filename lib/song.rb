92d29e7964e94494adbc1b1c0955da653aab982e
Dalya Eidda <dalia.eidda@gmail.com>
2019-01-11 22:50:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
     new_song
   end 
   
-  def self.create_from_filename(name)
+  def self.create_from_filename(filename)
     new_file = filename.split(" - ")
     artist = new_file[0]
     song = new_file[1].chomp(".mp3")
