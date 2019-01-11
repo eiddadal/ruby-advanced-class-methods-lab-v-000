2d57de367bfdff42a9174220a4092de0bba45120
Dalya Eidda <dalia.eidda@gmail.com>
2019-01-11 22:33:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,43 @@ class Song
   def self.all
     @@all
   end
-
+  
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = Song.create
+    
+    @@all << song
+  end 
+  
+  def self.new_by_name(name)
+    @name= name 
+    
+  end 
+  
+  def self.create_by_name
+  end 
+  
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+  end 
+  
+  def self.alphabetical(name)
+  end 
+  
+  def self.new_from_filename(name)
+  end 
+  
+  def self.create_from_filename(name)
+  end 
+  
+  def self.destroy_all 
+    @@all.clear
+  end 
 
 end
