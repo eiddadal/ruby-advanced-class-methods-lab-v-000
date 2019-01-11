969a9cd246a1d7703af462a40760071814115b20
Dalya Eidda <dalia.eidda@gmail.com>
2019-01-11 22:46:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
     @@all.sort_by {|song| song.name}
   end 
   
-  def self.new_from_filename(name)
+  def self.new_from_filename(filename)
     song = self.new 
     rows = filename.split("-")
     songs = rows.collect do |row|
